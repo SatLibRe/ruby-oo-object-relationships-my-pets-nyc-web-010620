d9c87d10e10c67291e4de0a42a8f0e837fe9e7f0
Miles Higbie <miles.higbie@gmail.com>
2020-01-13 21:53:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 class Dog 
+  attr_reader :name
+  attr_accessor :mood, :owner
   
+  @@all = []
   
+    def initialize(name,owner) 
+      @name = name
+      @owner = owner
+      @mood = "nervous"
+      @@all.push(self)
+    end 
+    
+    def self.all 
+      @@all
+    end 
 end 
