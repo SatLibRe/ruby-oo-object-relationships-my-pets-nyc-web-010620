0be5aa241ab270f4fe7c6281be69c86e197a2872
Miles Higbie <miles.higbie@gmail.com>
2020-01-13 21:47:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,2 +1,11 @@
 class Owner 
+  attr_reader :name, :species
+    def initialize(name) 
+      @name = name 
+      @species = species
+    end 
+    
+    def say_species 
+      ""
+    end 
 end 
