81ad4587dcbe4a24c547b3e9ad867c85c63538fb
Miles Higbie <miles.higbie@gmail.com>
2020-01-13 21:52:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,2 +1,25 @@
 class Cat  
+  attr_reader :name, :owner
+  attr_accessor :mood
+  
+  @@all = []
+  
+    def initialize(name) 
+      @name = name 
+      @mood = "nervous"
+      @@all.push(self)
+    end 
+    
+    def self.all 
+      @@all
+    end 
+    
+    # def self.count 
+    #   @@all.count
+    # end 
+    
+    # def self.reset_all 
+    #   @@all.clear
+    # end 
+  
 end 
